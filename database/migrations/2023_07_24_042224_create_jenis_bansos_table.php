594a561e68c1636b9de22210ea3bfafef5690e78
Hansen Makangiras <hansenmakangiras@gmail.com>
2023-10-23 16:25:34 +0800

    Add Str class to migrations, fix table name in down method
    Added `Str` class to `families`, `rastra` and `jenis_bansos` migrations files. This class provides string manipulation functions making the code cleaner and more efficient.
    
    Also, corrected the table name in the `down` method of `jenis_bansos` migration. It was wrongly trying to drop `status_bansos`. Corrected it to `jenis_bansos` to prevent any issues during rollback operations.

--- database/migrations/2023_07_24_042224_create_jenis_bansos_table.php
+++ database/migrations/2023_07_24_042224_create_jenis_bansos_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('status_bansos');
+        Schema::dropIfExists('jenis_bansos');
     }
 };
